e3528cc43d051ab68c9559b3cbfdbd4e78ce3721
Earth <31960699+Ethzth@users.noreply.github.com>
2022-01-13 07:53:49 +0700

    M-main

---
+++ src/data.js
@@ -0,0 +1,289 @@
+export const newsSlideItems = [
+  {
+    id: 1,
+    img: "assets/3BB_News1.jpg",
+  },
+  {
+    id: 2,
+    img: "assets/3BB_News2.jpg",
+  },
+  {
+    id: 3,
+    img: "assets/3BB_News3.jpg",
+  },
+];
+
+export const packages = [
+  {
+    id: 1,
+    img: "assets/3BB_package1.jpeg",
+  },
+  {
+    id: 2,
+    img: "assets/3BB_package2.jpg",
+  },
+];
+
+export const promotions = [
+  {
+    id: 1,
+    img: "assets/Promotions/3BB_promotion1.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 2,
+    img: "assets/Promotions/3BB_promotion2.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 3,
+    img: "assets/Promotions/3BB_promotion3.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 4,
+    img: "assets/Promotions/3BB_promotion4.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 5,
+    img: "assets/Promotions/3BB_promotion5.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 6,
+    img: "assets/Promotions/3BB_promotion6.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 7,
+    img: "assets/Promotions/3BB_promotion7.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 8,
+    img: "assets/Promotions/3BB_promotion8.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 9,
+    img: "assets/Promotions/3BB_promotion9.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 10,
+    img: "assets/Promotions/3BB_promotion10.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 11,
+    img: "assets/Promotions/3BB_promotion11.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 12,
+    img: "assets/Promotions/3BB_promotion12.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+];
+
+export const privilege = [
+  {
+    id: 1,
+    img: "assets/Privilege/3BB_privilege1.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 2,
+    img: "assets/Privilege/3BB_privilege2.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 3,
+    img: "assets/Privilege/3BB_privilege3.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 4,
+    img: "assets/Privilege/3BB_privilege4.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 5,
+    img: "assets/Privilege/3BB_privilege5.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 6,
+    img: "assets/Privilege/3BB_privilege6.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 7,
+    img: "assets/Privilege/3BB_privilege7.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 8,
+    img: "assets/Privilege/3BB_privilege8.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 9,
+    img: "assets/Privilege/3BB_privilege9.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 10,
+    img: "assets/Promotions/3BB_promotion10.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 11,
+    img: "assets/Promotions/3BB_promotion11.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 12,
+    img: "assets/Promotions/3BB_promotion12.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+];
+
+export const rewards = [
+  {
+    id: 1,
+    img: "assets/Rewards/3BB_reward1.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 2,
+    img: "assets/Rewards/3BB_reward2.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 3,
+    img: "assets/Rewards/3BB_reward3.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 4,
+    img: "assets/Rewards/3BB_reward4.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 5,
+    img: "assets/Rewards/3BB_reward5.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 6,
+    img: "assets/Rewards/3BB_reward6.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 7,
+    img: "assets/Promotions/3BB_promotion7.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 8,
+    img: "assets/Promotions/3BB_promotion8.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 9,
+    img: "assets/Promotions/3BB_promotion9.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 10,
+    img: "assets/Promotions/3BB_promotion10.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 11,
+    img: "assets/Promotions/3BB_promotion11.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 12,
+    img: "assets/Promotions/3BB_promotion12.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+];
+
+export const events = [
+  {
+    id: 1,
+    img: "assets/Events/3BB_event1.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 2,
+    img: "assets/Events/3BB_event2.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 3,
+    img: "assets/Events/3BB_event3.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 4,
+    img: "assets/Events/3BB_event4.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 5,
+    img: "assets/Events/3BB_event5.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+  {
+    id: 6,
+    img: "assets/Events/3BB_event6.jpg",
+    title: "Lorem",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nam doloremque delectus iure facilis ipsam cum rerum excepturi magnam omnis, ipsum deleniti. Nesciunt, eaque accusamus aliquid dolores soluta laudantium architecto!",
+  },
+];
